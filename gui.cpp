a20b35a2637f4b8087ec329d50f05ea0debfda9b
Merituum <bartolopoland@gmail.com>
2023-12-01 17:25:50 +0100

    zaczynam robic GUI

---
+++ gui.cpp
@@ -0,0 +1,5 @@
+//
+// Created by basiw on 01.12.2023.
+//
+
+#include "gui.h"
---
+++ gui.h
@@ -0,0 +1,14 @@
+//
+// Created by basiw on 01.12.2023.
+//
+
+#ifndef INFA_PROJ_2023_GUI_H
+#define INFA_PROJ_2023_GUI_H
+
+
+class gui {
+
+};
+
+
+#endif //INFA_PROJ_2023_GUI_H
